02ca693665aee641839515344bb8a0ec6c8c750d
pilou <p-l.b@hotmail.com>
2023-05-16 10:47:14 +0200

    changed repo

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -15,31 +15,30 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getCupcakes(): Observable<Cupcake[]> {
-    if(this._cupcakesSubject$.value.length) { // > 0
+    if (this._cupcakesSubject$.value.length) { // > 0
       console.log("OMG déjà une valeur");
       return this._cupcakesSubject$.asObservable();
     } else {
-       return this.http.get<any>(this.BASE_URL)
-       .pipe(
-        tap((cupcakes: Cupcake[]) => {
-          this._cupcakesSubject$.next(cupcakes);
-          console.log("fetch de la data");      
-        }),
-        switchMap(() => this._cupcakesSubject$.asObservable())
-      );
+      return this.http.get<any>(this.BASE_URL)
+        .pipe(
+          tap((cupcakes: Cupcake[]) => {
+            this._cupcakesSubject$.next(cupcakes);
+            console.log("fetch de la data");
+          }),
+          switchMap(() => this._cupcakesSubject$.asObservable())
+        );
     }
   }
 
   addCupcake(): void {
-    const header = { 'content-type' : 'application/json'};
+    const header = { 'content-type': 'application/json', 'authorization': 'L?EVPZE?' };
     const body = JSON.stringify(new Cupcake("my new cupcake", 3.00));
     this.http.post<Cupcake>(this.BASE_URL, body, {
       headers: header
     })
-    .subscribe((res) => {
-      this._cupcakesSubject$.next([...this._cupcakesSubject$.value, res])
-    })
+      .subscribe((res) => {
+        this._cupcakesSubject$.next([...this._cupcakesSubject$.value, res])
+      })
   }
-
 
 }
